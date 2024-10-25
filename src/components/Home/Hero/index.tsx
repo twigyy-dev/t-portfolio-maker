902e8acefc065f87ecd6867a666a3692e47d3889
Tamalika Bhowal <tamalikabhowal@gmail.com>
2024-10-25 22:27:22 +0530

    hero section done

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -2,29 +2,35 @@ import Image from "next/image";
 import React from "react";
 import brandData from "./brandData";
 import Link from "next/link";
+import "../../../app/sass/herosection.scss";
+//import mainbackgroundimage from "/images/hero/mainbackgroundimage.png"
 
 const Hero = () => {
   return (
     <section>
-		
-      <div className="text-center px-5 w-full ">
-        <h1 className="text-[50px] text-[#B7B7B7] font-extrabold ">
-          You{""}
-          <span className="text-[60px] text-[#09D1D5] font-extrabold">
-            think{" "}
-          </span>
-          <span className="text-[60px] text-[#09D1D5] font-extrabold">
-            big{""}
-          </span>
-        </h1>
-        <h1 className="text-[50px] text-[#B7B7B7] font-extrabold ">
-          We make it {""}
-          <span className="text-[60px] text-[#09D1D5] font-extrabold">
-            possible{" "}
-          </span>
-        </h1>
+      <div className=" main_image ">
+        <div className="main_text">
+          <h1 className="text-[108px] text-[#B7B7B7] font-extrabold ">
+            You{" "}
+            <span className="text-[108px] text-[#09D1D5] font-extrabold">
+              think{" "}
+            </span>
+            <span className="text-[108px] text-[#B7B7B7] font-extrabold">
+              big{""}
+            </span>
+          </h1>
+          <h1 className="text-[108px] text-[#B7B7B7] font-extrabold ">
+            We make it {""}
+            <span className="text-[108px] text-[#09D1D5] font-extrabold">
+              possible{" "}
+            </span>
+          </h1>
+        </div>
+        <div className="w-full flex justify-center mt-30">
+          <button className="explore_btn">Explore</button>
+        </div>
 
-        {/* <h1 className="text-[60px] text-[#09D1D5] font-extrabold">think</h1> */}
+       
       </div>
     </section>
     // <section className='relative z-1 overflow-hidden pb-17.5 pt-30 lg:pb-20 lg:pt-30 xl:pb-25 xl:pt-[170px]'>
