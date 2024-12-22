16db1d4abb8fb764a399ca37968dc7ef95150ef5
Swarup Das <swarupd049@gmail.com>
2024-12-22 23:31:59 +0530

    updated navbar to add dropdown for services

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,29 +1,50 @@
 import React from "react";
-import "../../app/sass/navstyle.scss";
 import Link from "next/link";
-import Section from "../Common/Section";
+import "../../app/sass/navstyle.scss";
 
-// Nvigation bar section that has different sections for each section
 const Navbar = () => {
   return (
     <section className="nav">
       <ul className="nav-child border-rounded-white">
         <li>
-          <Link href="/" className="nav-h1">Home</Link>
+          <Link href="/" className="nav-h1">
+            Home
+          </Link>
+        </li>
+        <li className="nav-dropdown">
+          <span className="nav-h1">Services</span>
+          <ul className="dropdown-menu">
+            <li>
+              <Link href="/services/web-development" className="dropdown-item">
+                Web Development
+              </Link>
+            </li>
+            <li>
+              <Link href="/app-development" className="dropdown-item">
+                App Development
+              </Link>
+            </li>
+            <li>
+              <Link href="/digital-marketing" className="dropdown-item">
+                Digital Marketing
+              </Link>
+            </li>
+          </ul>
         </li>
         <li>
-          <Link href="/contact-us" className="nav-h1">Services</Link>{" "}
+          <Link href="/pricing" className="nav-h1">
+            Pricing
+          </Link>
         </li>
         <li>
-          <Link href="/contact-us" className="nav-h1">Pricing</Link>
-        </li>
-        <li>
-          <Link href="/contact-us" className="nav-h1">FAQs</Link>{" "}
+          <Link href="/faqs" className="nav-h1">
+            FAQs
+          </Link>
         </li>
         <li>
           <Link href="/contact-us" className="nav-h1">
             Contact Us
-          </Link>{" "}
+          </Link>
         </li>
       </ul>
     </section>
